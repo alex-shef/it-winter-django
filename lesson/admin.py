e6153baea373a5900b8182278b6027527a3fde8b
Maxim_Belov <belovmd@gmail.com>
2020-02-19 12:53:26 +0300

    First iteration

---
+++ lesson/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+
+# admin.site.register(models.Material)
+
+@admin.register(models.Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'material_type', 'status', 'publish')
+    list_filter = ('status', 'created', 'publish', 'material_type')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
---
+++ lesson/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Material(models.Model):
+
+    MATERIAL_TYPES = (
+        ('theory', 'Theoretical'),
+        ('practice', 'Practical'),
+    )
+
+    STATUS_TYPES = (
+        ('private', 'Draft'),
+        ('public', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now())
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    material_type = models.CharField(max_length=20,
+                                     choices=MATERIAL_TYPES,
+                                     default='theory')
+
+    status = models.CharField(max_length=20,
+                              choices=STATUS_TYPES,
+                              default='private')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='user_materials')
+
+    # class Meta:
+    #     ordering = ('-publish', )
+    #
+    # def __str__(self):
+    #     return self.title
