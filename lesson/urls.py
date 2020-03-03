506aff476f025be742ed7a2e20f1b40cde53684b
Maxim Belov <belovmd@gmail.com>
2020-03-03 19:09:51 +0300

    Add forms

--- lesson/urls.py
+++ lesson/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
          views.material_details,
          name='material_details'),
     path('<int:material_id>/share/', views.share_material,
-         name='share_material')
+         name='share_material'),
+    path('create/', views.create_form,
+         name='create_form')
 ]
